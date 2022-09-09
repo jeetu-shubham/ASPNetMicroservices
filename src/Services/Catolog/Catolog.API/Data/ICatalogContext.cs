03b1d8b2861854481de546e79199117720523dc2
Shubham Sharma <shubhamsharma40@outlook.com>
2022-09-09 10:54:04 +0530

    initial change

---
+++ src/Services/Catolog/Catolog.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catolog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catolog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
