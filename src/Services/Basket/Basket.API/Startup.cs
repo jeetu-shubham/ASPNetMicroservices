7c50a0691a7ca7815bcb1518fd25b2697b5e84e1
Shubham Sharma <shubhamsharma40@outlook.com>
2022-09-21 11:56:26 +0530

    Basket API added, docker & docker compose files added for catalog API

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,5 +1,3 @@
-using Catolog.API.Data;
-using Catolog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -13,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Catolog.API
+namespace Basket.API
 {
     public class Startup
     {
@@ -27,15 +25,12 @@ namespace Catolog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddScoped<ILogger, ILogger>();
-            
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catolog.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
             });
-            services.AddScoped<ICatalogContext, CatalogContext>();
-            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +40,7 @@ namespace Catolog.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catolog.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
             }
 
             app.UseRouting();
---
+++ src/Services/Catolog/Catolog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using Catolog.API.Data;
+using Catolog.API.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catolog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ICatalogContext _context { get; }
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task createProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+            var updatedResutl = await _context.Products.DeleteOneAsync(filter);
+            return updatedResutl.IsAcknowledged && updatedResutl.DeletedCount > 0;
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updatedResutl = await _context.Products.ReplaceOneAsync(p => p.Id == product.Id, product);
+            return updatedResutl.IsAcknowledged && updatedResutl.ModifiedCount > 0;
+        }
+    }
+}
